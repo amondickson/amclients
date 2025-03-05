c762a71f44deea2211f32801aef6a339a7c4be7b
Amon Dickson <amondickson281@gmail.com>
2025-03-05 02:18:55 +0000

    Initial commit

--- turorial1/turorial1/settings.py
+++ turorial1/turorial1/settings.py
@@ -120,9 +120,4 @@ STATICFILES_DIRS = [BASE_DIR/'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
 django_heroku.settings(locals())
